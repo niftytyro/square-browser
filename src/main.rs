1897e8b9a8b95dd9d6c91cd1b694aed2f04815df
Udasi Tharani <udasitharani@gmail.com>
2023-01-10 00:38:46 +0530

    fetching page

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,64 @@
 use std::io::{self, Read, Write};
-use std::net::{SocketAddr, TcpStream};
+use std::net::TcpStream;
 
 // TODO Add read/write timeouts
 // TODO Kill connections after unresponsive for too long
 // TODO Error handling
 // TODO blocking vs non-blocking
+// TODO handle ports and "localhost"
+
+const MAX_BUFFER_LENGTH: usize = 1024;
 
 fn main() {
-    let url = recieve_url_input();
+    let url = read_url_input();
 
-    let (host, path) = parse_url(&url);
+    let (scheme, host, path) = parse_url(&url);
 
-    println!("Host: {}\nPath: {}", host, path);
+    let mut socket = establish_connection(scheme, host);
+
+    let response = fetch_page(&mut socket, path, host);
+
+    println!("\n-------------------------------\n");
+    println!("{}", response);
 }
 
-fn parse_url(url: &String) -> (&str, &str) {
+fn fetch_page(socket: &mut TcpStream, path: &str, host: &str) -> String {
+    socket
+        .write(format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", path, host).as_bytes())
+        .expect("Failed to write to socket.");
+    socket
+        .flush()
+        .expect("Something wrong while flushing stream.");
+
+    read_response(socket)
+}
+
+fn read_response(socket: &mut TcpStream) -> String {
+    let mut buf = [0u8; MAX_BUFFER_LENGTH];
+
+    let mut response_bytes: Vec<u8> = vec![];
+
+    loop {
+        let bytes_read = socket.read(&mut buf).expect("Failed to read stream");
+
+        response_bytes.extend_from_slice(&buf[..bytes_read]);
+
+        if bytes_read < MAX_BUFFER_LENGTH {
+            break;
+        }
+    }
+
+    let response = String::from_utf8(response_bytes).expect("Failed to convert to utf-8.");
+
+    response
+}
+
+fn establish_connection(_: &str, host: &str) -> TcpStream {
+    let socket = TcpStream::connect(format!("{}:{}", host, 80)).expect("Failed to connect.");
+    socket
+}
+
+fn parse_url(url: &String) -> (&str, &str, &str) {
     let url_vec: Vec<&str> = url.splitn(2, "://").collect();
     let scheme = url_vec[0];
     let rest: Vec<&str> = url_vec[1].splitn(2, "/").collect();
@@ -22,10 +66,10 @@ fn parse_url(url: &String) -> (&str, &str) {
     let host = rest[0];
     let path = if rest.len() > 1 { rest[1] } else { "/" };
 
-    (host, path)
+    (scheme, host, path)
 }
 
-fn recieve_url_input() -> String {
+fn read_url_input() -> String {
     let mut url = String::new();
     io::stdin().read_line(&mut url).expect("Failed to read");
     let url = String::from(url.trim());
