c922574ac54b17be687c987ca9526875af97fd5f
Udasi Tharani <udasitharani@gmail.com>
2025-09-22 16:32:22 +0530

    sending bytes over the wire

--- src/main.c
+++ src/main.c
@@ -6,50 +6,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 
 int main(int argc, char *argv[]) {
+
+  struct addrinfo hints;
+  struct addrinfo *server_address;
+  short int status;
+  int socket_fd;
+
   if (argc != 2) {
     printf("Usage: square <url>\n\n");
     exit(1);
   }
 
-  struct addrinfo hints;
-  struct addrinfo *serverInfo;
-  short int status;
-
   memset(&hints, 0, sizeof hints);
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_PASSIVE;
 
-  status = getaddrinfo(argv[1], "3490", &hints, &serverInfo);
+  status = getaddrinfo(argv[1], "80", &hints, &server_address);
 
   if (status != 0) {
     fprintf(stderr, "Could not getaddressinfo(): %s\n", gai_strerror(status));
     exit(2);
   }
 
-  if (serverInfo->ai_family == AF_INET) {
+  if (server_address->ai_family == AF_INET) {
     void *addr;
     char buffer[INET_ADDRSTRLEN];
 
-    addr = &((struct sockaddr_in *)(serverInfo->ai_addr))->sin_addr;
+    addr = &((struct sockaddr_in *)(server_address->ai_addr))->sin_addr;
 
     inet_ntop(AF_INET, addr, buffer, INET_ADDRSTRLEN);
 
     printf("IPv4 Address: %s\n", buffer);
 
-  } else if (serverInfo->ai_family == AF_INET6) {
+  } else if (server_address->ai_family == AF_INET6) {
     void *addr;
     char buffer[INET6_ADDRSTRLEN];
 
-    addr = &((struct sockaddr_in6 *)(serverInfo->ai_addr))->sin6_addr;
+    addr = &((struct sockaddr_in6 *)(server_address->ai_addr))->sin6_addr;
 
     inet_ntop(AF_INET6, addr, buffer, INET6_ADDRSTRLEN);
 
     printf("IPv6 Address: %s\n", buffer);
   }
 
-  freeaddrinfo(serverInfo);
+  printf("Creating a socket now.\n");
+
+  socket_fd = socket(server_address->ai_family, server_address->ai_socktype,
+                     server_address->ai_protocol);
+
+  printf("Connection attempt started.\n");
+
+  status =
+      connect(socket_fd, server_address->ai_addr, server_address->ai_addrlen);
+
+  printf("status: %d", status);
+  if (status != 0) {
+    fprintf(stderr, "Failed to bind socket: %s", gai_strerror(status));
+  }
+
+  freeaddrinfo(server_address);
   return 0;
 }
+
+void split_url(char *url) {}
+
+void connect_to_host() {}
+
+char *form_http_request() { return ""; }
