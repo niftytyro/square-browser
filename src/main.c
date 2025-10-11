df03de0c19e93c3f1c40230a00f3103d63ec5d77
Udasi Tharani <udasitharani@gmail.com>
2025-10-11 17:05:02 +0530

    refactor

--- src/main.c
+++ src/main.c
@@ -13,9 +13,9 @@
 #include "./network/request.h"
 
 int main(int argc, char *argv[]) {
-  struct addrinfo *server_address;
   int socket_fd;
-  char *response;
+  char *request, *response;
+  struct addrinfo *server_address;
 
   if (argc != 2) {
     printf("Usage: square <url>\n\n");
@@ -27,8 +27,11 @@ int main(int argc, char *argv[]) {
 
   split_url(argv[1], &url);
   make_addr_from_URL(&url, &server_address);
+
   socket_fd = open_connection(server_address);
-  char *request = form_http_request(&url, "GET", "1.0");
+
+  request = form_http_request(&url, "GET", "1.0");
+
   send_request(socket_fd, request);
   read_response(socket_fd, &response);
 
